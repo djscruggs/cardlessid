34aa36f6b63cbb72c9c3279eb1cc901bf8e4c454
Derek Scruggs <me@derekscruggs.com>
2025-09-13 16:19:13 -0500

    more iframe fixes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
       <div className="flex flex-col justify-start  max-w-xl  border">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/mZGn2YGVDck?si=MI7PkER1ci2eNnMo" title="What is Cardless ID" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+        About
       </div>
     </>
   )
--- src/pages/What.tsx
+++ src/pages/What.tsx
@@ -4,7 +4,16 @@ const What = () => {
   return (
     <>
       <div className="flex flex-col justify-start  max-w-xl  border">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/mZGn2YGVDck?si=MI7PkER1ci2eNnMo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="flex flex-col justify-start  max-w-xl  border">
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/mZGn2YGVDck?si=MI7PkER1ci2eNnMo" 
+          title="What is Cardless ID?" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen
+        ></iframe>
+      </div>
       </div>
     </>
   )
