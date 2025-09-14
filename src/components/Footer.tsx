df49206ffce713afc8d8f86f958173134571e3b2
Derek Scruggs <me@derekscruggs.com>
2025-09-14 08:49:04 -0500

    added linkedin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className='mt-20'>
+    <footer className='my-2'>
       <p className='text-sm'>&copy; 2025 Cardless ID. All rights reserved.</p>
+      <div className="flex justify-center my-4">
+        <a href="https://www.linkedin.com/company/cardless-id" target="_blank" rel="noopener noreferrer">
+          <img src="/linkedin.png" alt="LinkedIn" className="h-6 w-6" />
+        </a>
+      </div>
+
     </footer>
   );
 };
