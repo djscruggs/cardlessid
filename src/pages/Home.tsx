2725958e887a4d3543ae4c7b6f06990469f72671
Derek Scruggs <me@derekscruggs.com>
2025-09-20 16:23:52 -0500

    moved main diagram into carousel for mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import usePageTitle from "../hooks/usePageTitle";
 
@@ -11,6 +11,11 @@ const Home: React.FC = () => {
       "_blank"
     );
   };
+  // const isMobile = Boolean(
+  //   navigator?.userAgent?.match(
+  //     /Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile|WPDesktop/i
+  //   )
+  // );
   const navigate = useNavigate();
   const goToDemo = () => {
     navigate("/demo");
@@ -21,9 +26,9 @@ const Home: React.FC = () => {
         <div className="px-10 text-gray-800  text-2xl  flex flex-col items-center space-y-2">
           <div className="max-w-3xl text-left mt-4 space-y-4">
             <p>
-              24 US states and most of Europe require adult sites to verify age.
-              Children should never have access to sexually explicit material.
-              However, the verification process should be{" "}
+              24 US states and several European countries require adult sites to
+              verify age. Children should never have access to sexually explicit
+              material. However, the verification process should be{" "}
               <strong>free, private and portable accross all web sites</strong>.
             </p>
             {/* <div className="ml-10 text-md">
@@ -47,7 +52,13 @@ const Home: React.FC = () => {
               We partner with content companies, technology providers, media and
               regulators to make this possible.
             </p>
-            <img src="/diagram.png" className="max-w-4xl" />
+            <div className="md:hidden">
+              <Carousel />
+            </div>
+            <img
+              src="/diagrams/diagram-full.png"
+              className="hidden md:block max-w-4xl md:pr-50"
+            />
             <div className="flex justify-center items-center space-x-4 my-10">
               <button
                 className="bg-logoblue p-2 md:p-4 text-white text-sm sm:text-2xl rounded-full cursor-pointer"
@@ -73,6 +84,22 @@ const Home: React.FC = () => {
         </div>
       </div>
     </>
+  );
+};
+
+const Carousel = () => {
+  return (
+    <div className="carousel carousel-center bg-white rounded-box  space-x-4 p-4">
+      <div className="carousel-item bg-white">
+        <img src="/diagrams/diagram1.png" className="rounded-box pb-120" />
+      </div>
+      <div className="carousel-item">
+        <img src="/diagrams/diagram2.png" />
+      </div>
+      <div className="carousel-item bg-white">
+        <img src="/diagrams/diagram3.png" className="rounded-box pb-60 " />
+      </div>
+    </div>
   );
 };
 
