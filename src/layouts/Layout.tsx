abf5b32e73e3052420feefdc2a7a6fb1dcd80472
Derek Scruggs <me@derekscruggs.com>
2025-09-24 08:19:03 -0500

    removed photo of dj, turned home link on demo pages to button

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,32 +9,36 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const location = useLocation();
-  const isDemo = location.pathname.includes('demo')
-  const showHeader = location.pathname != '/' && !isDemo
-  const showFooter = !isDemo
-  const isPhone = location.pathname.includes('phone')
+  const isDemo = location.pathname.includes("demo");
+  const showHeader = location.pathname != "/" && !isDemo;
+  const showFooter = !isDemo;
+  const isPhone = location.pathname.includes("phone");
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
 
   return (
-    <div className='flex flex-col w-screen h-screen'>
-      {!isDemo && !isPhone &&
-        <Header />
-      }
-      {isDemo && !isPhone &&
+    <div className="flex flex-col w-screen h-screen">
+      {!isDemo && !isPhone && <Header />}
+      {isDemo && !isPhone && (
         <div className="pt-8">
-          <a href="/" className="absolute top-24 left-4 md:left-30 text-logoblue hover:underline text-sm font-semibold">
+          <button
+            onClick={goHome}
+            className="absolute top-24 left-4 md:left-30 bg-logoblue text-white p-2 cursor-pointer rounded-md  text-sm font-semibold"
+          >
             &laquo; Home
-          </a>
+          </button>
           <img src="/logospicy.png" className="h-[160px] mx-auto block" />
         </div>
-      }
-      <main className={`w-full ${showHeader ? 'p-4' : 'p-0'} flex flex-col justify-start flex-grow`}>
+      )}
+      <main
+        className={`w-full ${showHeader ? "p-4" : "p-0"} flex flex-col justify-start flex-grow`}
+      >
         {children}
       </main>
-      {showFooter &&
-        <Footer />
-      }
+      {showFooter && <Footer />}
     </div>
-  
   );
 };
 
