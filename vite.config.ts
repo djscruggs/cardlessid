442fac433ad67b250e782de0d777e6a210a46e24
Derek Scruggs <me@derekscruggs.com>
2025-09-25 19:40:25 -0500

    config tweak

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
+  server: {
+    host: true,
+  },
 });
