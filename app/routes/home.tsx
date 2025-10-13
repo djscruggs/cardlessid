afda257086741520244f0bdc878b49398d2b45d6
Derek Scruggs <me@derekscruggs.com>
2025-10-13 11:13:02 -0500

    added headline to home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,6 +10,9 @@ const Home: React.FC = () => {
       <div className="flex-col w-full">
         <div className="px-10 text-gray-800  text-2xl  flex flex-col items-center space-y-2">
           <div className="max-w-3xl text-left mt-4 space-y-4">
+            <h2 className="text-4xl text-logoblue">
+              Free, Private, Portable Age Verification
+            </h2>
             <p>
               24 US states and several European countries require adult sites to
               verify age. Children should never have access to sexually explicit
