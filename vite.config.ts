f744d2c28577bcbf03e88bb8f047b7d066b07794
Derek Scruggs <me@derekscruggs.com>
2025-10-17 08:57:07 -0500

    added vite-llms plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
+import llms from "vite-plugin-llms";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson(), llms()],
   server: {
     host: true,
     allowedHosts: [".ngrok-free.app"],
